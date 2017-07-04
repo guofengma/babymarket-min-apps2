8a5ddb06fdf17e4afa6cb1edd7b1a5370b3d677d
pyf <498387192@qq.com>
2017-07-04 16:07:43 +0800

    漏传文件

---
+++ pages/my/withdraw/pay-password-set/pay-password-set.js
@@ -0,0 +1,79 @@
+// pay-password-set.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+      datas: [
+          {
+              'inputName': 'name',
+              'title': '新密码:',
+              'placeholder': '请输入新密码',
+              'value': ''
+          },
+          {
+              'inputName': 'account',
+              'title': '确认密码:',
+              'placeholder': '请再次输入新密码',
+              'value': ''
+          }
+      ],
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
